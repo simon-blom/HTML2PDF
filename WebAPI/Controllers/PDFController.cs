95700ec077687f42083db8222d0019ada439cafd
Simon Blom <simon_blom@hotmail.com>
2020-05-18 19:31:00 +0200

    Boilerplate

---
+++ WebAPI/Controllers/PDFController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PDFController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<PDFController> _logger;
+
+        public PDFController(ILogger<PDFController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public async Task<byte[]> Create()
+        {
+            return new byte[0];
+        }
+    }
+}
